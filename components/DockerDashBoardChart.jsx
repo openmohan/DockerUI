32d47195de81466715890b68ab205076d57f5bc6
Mohan Prasath <mohanprasath@live.com>
2016-11-16 19:59:06 +0530

    Chart data added

--- components/DockerDashBoardChart.jsx
+++ components/DockerDashBoardChart.jsx
@@ -6,24 +6,40 @@ var BarChart = require("react-chartjs").Bar;
 var DockerDashBoardChart = React.createClass({
 	render : function(){
 		var labels = [];
+		var dataSet = [];
 		var chartcontentdata=[];
+		var datasetsingle = {};
 		var data = this.props.data || [];
 		data.forEach(function(data){
 			var lab = data.containerID.Id;
 			lab = lab.slice(0,5)
 			labels.push(lab);
 			chartcontentdata.push(data.networks.rx_bytes);
+			datasetsingle = {label : lab,data :data.networks.rx_bytes }
+			dataSet.push(datasetsingle);
 		})
+		console.log(labels);
+		console.log(chartcontentdata);
 		var chartData = {
 			labels : labels,
 			datasets : [
-				{
+				{		
 						fillColor : "#0088ff",
 						strokeColor : "rgba(0,0,0,0)",
 						highlightFill: "#0070ff",
 						data: chartcontentdata
 				}
 			]
+		};
+		var data = {
+		    labels: ["January", "February", "March", "April", "May", "June", "July"],
+		    datasets: [
+		        {
+		            label: "My First dataset",
+		            borderWidth: 1,
+		            data: [65, 59, 80, 81, 56, 55, 40],
+		        }
+		    ]
 		};
 		var chartOptions =  {
 			        		responsive:true,
@@ -40,7 +56,7 @@ var DockerDashBoardChart = React.createClass({
 		   }
 		return(
 		
-			<BarChart className="" data={chartData} options={chartOptions} chartType="horizontalBar" width={100} height={80} />
+			<BarChart className="" data={chartData} options={chartOptions} type="horizontalBar" width={100} height={80} />
 			
 			) 
 	}
